1f16f59f161c2cd8de16338bb66cf617e9c56253
ljcheng-tech <johnny.cheng919@gmail.com>
2025-05-27 22:16:46 -0600

    init test for oidc

---
+++ README.md
@@ -0,0 +1,1 @@
+### Url: ljcheng.kubesources.com
---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+const words = [
+  { text: "Automation", imgPath: "/images/automation.gif" },
+  { text: "Security", imgPath: "/images/cloud-security.png" },
+  { text: "System", imgPath: "/images/system-info.png" },
+  { text: "Environments", imgPath: "/images/public-cloud.png" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 15, suffix: "+", label: "Years of Experience" },
+  { value: 200, suffix: "+", label: "Satisfied Clients" },
+  { value: 72, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+export {
+  words,
+  counterItems,
+  // abilities,
+  // logoIconsList,
+  // expCards,
+  // expLogos,
+  // testimonials,
+  // socialImgs,
+  // techStackIcons,
+  // techStackImgs,
+  // navLinks,
+};
