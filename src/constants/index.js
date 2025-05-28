c86759d1c8c8316916563ecce458668273299368
ljcheng-tech <johnny.cheng919@gmail.com>
2025-05-28 01:00:33 -0600

    work section almost done

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,10 +10,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 72, suffix: "+", label: "Completed Projects" },
+  { value: 7, suffix: "+", label: "Years of Experience" },
+  { value: 43, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 200, suffix: "+", label: "Satisfied Clients" },
 ];
 
 export {
--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -11,37 +11,37 @@ const AppShowcase = () => {
   const libraryRef = useRef(null);
   const ycDirectoryRef = useRef(null);
 
-  // useGSAP(() => {
-  //   // Animation for the main section
-  //   gsap.fromTo(
-  //     sectionRef.current,
-  //     { opacity: 0 },
-  //     { opacity: 1, duration: 1.5 }
-  //   );
+  useGSAP(() => {
+    // Animation for the main section
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
 
-  //   // Animations for each app showcase
-  //   const cards = [rydeRef.current, libraryRef.current, ycDirectoryRef.current];
+    // Animations for each app showcase
+    const cards = [rydeRef.current, libraryRef.current, ycDirectoryRef.current];
 
-  //   cards.forEach((card, index) => {
-  //     gsap.fromTo(
-  //       card,
-  //       {
-  //         y: 50,
-  //         opacity: 0,
-  //       },
-  //       {
-  //         y: 0,
-  //         opacity: 1,
-  //         duration: 1,
-  //         delay: 0.3 * (index + 1),
-  //         scrollTrigger: {
-  //           trigger: card,
-  //           start: "top bottom-=100",
-  //         },
-  //       }
-  //     );
-  //   });
-  // }, []);
+    cards.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+  }, []);
 
   return (
     <div id="work" ref={sectionRef} className="app-showcase">
@@ -49,16 +49,26 @@ const AppShowcase = () => {
         <div className="showcaselayout">
           <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde App Interface" />
+              <a
+                href="https://github.com/ljcheng999/capi-gitops"
+                alt="Click to visit downstream cluster manifests"
+                target="_blank"
+              >
+                <img
+                  src="/images/project1-capi-1.png"
+                  alt="capi-cluster-manifest"
+                />
+              </a>
             </div>
             <div className="text-content">
               <h2>
-                On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                CI/CD pipelines with Infrastrucure are great but GitOps
+                Declarative ways could be better!
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast,
-                user-friendly experience.
+                An Stack build with Cluster API (CAPI), ArgoCD (GitOps), & Git
+                for a fast Kubernetes clusters creation and DevOps-friendly
+                experience.
               </p>
             </div>
           </div>
@@ -66,19 +76,34 @@ const AppShowcase = () => {
           <div className="project-list-wrapper overflow-hidden">
             <div className="project" ref={libraryRef}>
               <div className="image-wrapper bg-[#FFEFDB]">
-                <img
-                  src="/images/project2.png"
-                  alt="Library Management Platform"
-                />
+                <a
+                  href="https://github.com/ljcheng999/k8s-jfrog-xray"
+                  alt="Click to visit Kubernetes Operator Jfrog Xray"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/project2-k8s-operator-1.png"
+                    alt="k8s-operator-jfrog-xray"
+                  />
+                </a>
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>Kubernetes Operator - Jfrog Xray</h2>
             </div>
 
             <div className="project" ref={ycDirectoryRef}>
               <div className="image-wrapper bg-[#FFE7EB]">
-                <img src="/images/project3.png" alt="YC Directory App" />
+                <a
+                  href="https://gitlab.com/jcheng-tech-919/k8s-deploy"
+                  alt="Click to visit Golang deploy script repo"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/project3-golang-deploy-2.png"
+                    alt="golang deployment script"
+                  />
+                </a>
               </div>
-              <h2>YC Directory - A Startup Showcase App</h2>
+              <h2>Application Deployment written by Go</h2>
             </div>
           </div>
         </div>
